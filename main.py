db73132e205ab1ffe29e94b8c88923e8b5824e39
sonkcode <mailforfeature@gmail.com>
2023-02-26 17:00:03 +0300

    fix timer and other

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 
 
@@ -13,8 +14,16 @@ class Quiz:
     def ask_question(self):
         print(self.question)
         for i in range(len(self.answers)):
-            print(str(i+1) + ") " + self.answers[i])
-        self.user_answer = input("Please enter the number of your answer: ")
+            print(str(i + 1) + ") " + self.answers[i])
+        while True:
+            try:
+                self.user_answer = int(input("Please enter the number of your answer: "))
+                if self.user_answer < 1 or self.user_answer > len(self.answers):
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number between 1 and " + str(
+                    len(self.answers)) + " or 'h' for a hint.")
 
     def check_answer(self):
         if int(self.user_answer) == self.correct_answer:
@@ -24,30 +33,38 @@ class Quiz:
             print("Incorrect. The correct answer was " + str(self.correct_answer) + ".")
 
     def remove_wrong_answer(self):
+        removed_answers = 0
         for i in range(len(self.answers)):
-            if i + 1 != self.correct_answer:
+            if i + 1 != self.correct_answer and removed_answers < 1:
                 self.answers[i] = ''
-                break
+                removed_answers += 1
+        self.answers = list(filter(None, self.answers))
         self.hint_used = True
+
 
 def start_quiz():
     q1 = Quiz("What is the capital of France?", ["Paris", "London", "Berlin"], 1)
     q2 = Quiz("What is the capital of Germany?", ["Paris", "Berlin", "Madrid"], 2)
     q3 = Quiz("What is the capital of Spain?", ["Rome", "Madrid", "Paris"], 2)
     questions = [q1, q2, q3]
-    print("Welcome to the quiz! You have 5 seconds to answer each question. Good luck!")
-    start_time = datetime.now()
+    print("Welcome to the quiz! You have 20 seconds to answer each question. Good luck!")
     hint_used = False
     for q in questions:
+        start_time = time.time()
         q.ask_question()
-        if (datetime.now() - start_time).total_seconds() > 20:
-            print("Time's up! No points awarded.")
-            break
-        if not hint_used:
-            print("Enter 'h' for a hint.")
         while True:
-            answer = q.user_answer.lower()
-            if answer == 'h' and not q.hint_used:
+            time_left = int(20 - (time.time() - start_time))
+            if time_left <= 0:
+                print("Time's up! No points awarded.")
+                break
+            print("Time left: {} seconds".format(time_left))
+            if not hint_used:
+                print("Enter 'h' for a hint.")
+            if isinstance(q.user_answer, int):
+                answer = str(q.user_answer)
+            else:
+                answer = q.user_answer.lower()
+            if answer == 'hint' and not q.hint_used:
                 q.remove_wrong_answer()
                 hint_used = True
                 print("One wrong answer has been removed.")
@@ -56,7 +73,10 @@ def start_quiz():
                 q.check_answer()
                 break
             else:
-                print("Invalid input. Please enter a number or 'h' for a hint.")
+                print("Invalid input. Please enter a number or 'hint' for a hint.")
+        if time_left <= 0:
+            continue
     print("You have finished the quiz! You have earned " + str(q1.points + q2.points + q3.points) + " points.")
+
 
 start_quiz()
